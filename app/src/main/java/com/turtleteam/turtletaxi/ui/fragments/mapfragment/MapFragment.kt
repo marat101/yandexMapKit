16946060d7e7f1c434f5cb6306b438d60be5caaa
marat <maratnv101@gmail.com>
2022-12-02 03:05:53 +0300

    init commit

--- app/src/main/java/com/turtleteam/turtletaxi/ui/fragments/mapfragment/MapFragment.kt
+++ app/src/main/java/com/turtleteam/turtletaxi/ui/fragments/mapfragment/MapFragment.kt
@@ -7,6 +7,7 @@ import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,50 +17,50 @@ import com.turtleteam.turtletaxi.ui.fragments.basefragment.BaseFragment
 import com.yandex.mapkit.Animation
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
-import com.yandex.mapkit.map.CameraPosition
-import com.yandex.mapkit.map.InputListener
+import com.yandex.mapkit.layers.GeoObjectTapListener
+import com.yandex.mapkit.map.*
 import com.yandex.mapkit.map.Map
-import com.yandex.mapkit.map.PlacemarkMapObject
 import com.yandex.runtime.ui_view.ViewProvider
+
 
 class MapFragment : BaseFragment<FragmentMapBinding>() {
 
     private lateinit var locationManager: LocationManager
     private lateinit var userLocation: PlacemarkMapObject
+    private lateinit var endPoint: PlacemarkMapObject
+    private lateinit var inputListener: InputListener
+    private lateinit var objectTapListener: GeoObjectTapListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        locationManager = requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        locationManager =
+            requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        inputListener = gerInputListener()
+        objectTapListener = getObjectTapListener()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        binding.mapview.map.addInputListener(object : InputListener{
-//            override fun onMapTap(p0: Map, p1: Point) {
-//                TODO("Not yet implemented")
-//            }
-//            override fun onMapLongTap(p0: Map, p1: Point) {
-//                TODO("Not yet implemented")
-//            }
-//
-//              НА ЭТО ПОКА ЗАБЕЙ
-//        })
+        userLocation = binding.mapview.map.mapObjects.addPlacemark(Point(0.0, 0.0),
+            ViewProvider(binding.userMarker))
+        endPoint = binding.mapview.map.mapObjects.addPlacemark(Point(0.0, 0.0),
+            ViewProvider(binding.finishMarker))
+
+        binding.mapview.map.addTapListener(objectTapListener)
+        binding.mapview.map.addInputListener(inputListener)
 
         if (ActivityCompat.checkSelfPermission(requireContext(),
                 Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
                 requireContext(),
                 Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
         ) {
-            // НА ЭТО ТОЖЕ ЗАБЕЙ
         }
         locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
             1000 * 10,
             10F,
             locationListener())
 
-        userLocation = binding.mapview.map.mapObjects.addPlacemark(Point(0.0, 0.0),
-            ViewProvider(binding.userMarker))
     }
 
     override fun onPause() {
@@ -91,10 +92,6 @@ class MapFragment : BaseFragment<FragmentMapBinding>() {
             locationManager.getLastKnownLocation(provider)?.let { showLocation(it) }
         }
 
-        override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
-            super.onStatusChanged(provider, status, extras)
-        }
-
         override fun onLocationChanged(p0: Location) {
             showLocation(p0)
         }
@@ -107,6 +104,30 @@ class MapFragment : BaseFragment<FragmentMapBinding>() {
             null)
         userLocation.geometry = Point(location.latitude, location.longitude)
     }
+
+    private fun gerInputListener(): InputListener = object : InputListener {
+        override fun onMapTap(p0: Map, p1: Point) {
+            binding.mapview.map.deselectGeoObject()
+            Log.e("ssss", p1.toString())
+            endPoint.geometry = p1
+        }
+
+        override fun onMapLongTap(p0: Map, p1: Point) {
+        }
+    }
+
+    private fun getObjectTapListener(): GeoObjectTapListener =
+        GeoObjectTapListener { p0 ->
+            val selectionMetadata: GeoObjectSelectionMetadata = p0
+                .geoObject
+                .metadataContainer
+                .getItem(GeoObjectSelectionMetadata::class.java)
+
+            binding.mapview.map.selectGeoObject(selectionMetadata.id,
+                selectionMetadata.layerId)
+
+            true
+        }
 
     override fun getViewBinding(
         inflater: LayoutInflater,
