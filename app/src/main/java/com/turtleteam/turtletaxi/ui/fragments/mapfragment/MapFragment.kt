b8d99f3c654f8bf4b73f9b4afcc4f0f2a8db3e14
marat <maratnv101@gmail.com>
2022-12-04 00:48:56 +0300

    Refactoring

--- app/src/main/java/com/turtleteam/turtletaxi/ui/fragments/mapfragment/MapFragment.kt
+++ app/src/main/java/com/turtleteam/turtletaxi/ui/fragments/mapfragment/MapFragment.kt
@@ -1,71 +1,92 @@
 package com.turtleteam.turtletaxi.ui.fragments.mapfragment
 
-import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
-import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.app.ActivityCompat
 import com.turtleteam.turtletaxi.databinding.FragmentMapBinding
 import com.turtleteam.turtletaxi.ui.fragments.basefragment.BaseFragment
 import com.yandex.mapkit.Animation
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.layers.GeoObjectTapEvent
 import com.yandex.mapkit.layers.GeoObjectTapListener
 import com.yandex.mapkit.map.*
 import com.yandex.mapkit.map.Map
 import com.yandex.runtime.ui_view.ViewProvider
 
 
-class MapFragment : BaseFragment<FragmentMapBinding>() {
+class MapFragment : BaseFragment<FragmentMapBinding>(), LocationListener, GeoObjectTapListener,
+    InputListener {
 
-    private lateinit var locationManager: LocationManager
     private lateinit var userLocation: PlacemarkMapObject
     private lateinit var endPoint: PlacemarkMapObject
-    private lateinit var inputListener: InputListener
-    private lateinit var objectTapListener: GeoObjectTapListener
+    private lateinit var locationManager: LocationManager
 
+    @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        locationManager =
-            requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        inputListener = gerInputListener()
-        objectTapListener = getObjectTapListener()
+        locationManager = context?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+            1000 * 10,
+            10F,
+            this)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        userLocation = binding.mapview.map.mapObjects.addPlacemark(Point(0.0, 0.0),
-            ViewProvider(binding.userMarker))
         endPoint = binding.mapview.map.mapObjects.addPlacemark(Point(0.0, 0.0),
             ViewProvider(binding.finishMarker))
 
-        binding.mapview.map.addTapListener(objectTapListener)
-        binding.mapview.map.addInputListener(inputListener)
+        userLocation = binding.mapview.map.mapObjects.addPlacemark(Point(0.0, 0.0),
+            ViewProvider(binding.userMarker))
 
-        if (ActivityCompat.checkSelfPermission(requireContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
-        ) {
-        }
-        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
-            1000 * 10,
-            10F,
-            locationListener())
+        binding.mapview.map.addTapListener(this)
+        binding.mapview.map.addInputListener(this)
+    }
 
+    @SuppressLint("MissingPermission")
+    override fun onProviderEnabled(provider: String) {
+        locationManager.getLastKnownLocation(provider)?.let { this }
+    }
+
+    override fun onLocationChanged(p0: Location) {
+        userLocation.geometry = Point(p0.latitude, p0.longitude)
+        binding.mapview.map.move(
+            CameraPosition(userLocation.geometry, 15.5f, 0.0f, 0.0f),
+            Animation(Animation.Type.LINEAR, 0.5F),
+            null)
+    }
+
+    override fun onMapTap(p0: Map, p1: Point) {
+        binding.mapview.map.deselectGeoObject()
+        endPoint.geometry = Point(p1.latitude, p1.longitude)
+    }
+
+    override fun onMapLongTap(p0: Map, p1: Point) {
+    }
+
+    override fun onObjectTap(p0: GeoObjectTapEvent): Boolean {
+        val selectionMetadata: GeoObjectSelectionMetadata = p0
+            .geoObject
+            .metadataContainer
+            .getItem(GeoObjectSelectionMetadata::class.java)
+
+        binding.mapview.map.selectGeoObject(selectionMetadata.id,
+            selectionMetadata.layerId)
+
+        return true
     }
 
     override fun onPause() {
         super.onPause()
-        locationManager.removeUpdates(locationListener())
+        locationManager.removeUpdates(this)
     }
 
     override fun onStop() {
@@ -79,55 +100,6 @@ class MapFragment : BaseFragment<FragmentMapBinding>() {
         MapKitFactory.getInstance().onStart()
         binding.mapview.onStart()
     }
-
-    private fun locationListener(): LocationListener = object : LocationListener {
-
-        override fun onProviderEnabled(provider: String) {
-            if (ActivityCompat.checkSelfPermission(requireContext(),
-                    Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                    requireContext(),
-                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
-            ) {
-            }
-            locationManager.getLastKnownLocation(provider)?.let { showLocation(it) }
-        }
-
-        override fun onLocationChanged(p0: Location) {
-            showLocation(p0)
-        }
-    }
-
-    fun showLocation(location: Location) {
-        binding.mapview.map.move(
-            CameraPosition(Point(location.latitude, location.longitude), 15.5f, 0.0f, 0.0f),
-            Animation(Animation.Type.SMOOTH, 0F),
-            null)
-        userLocation.geometry = Point(location.latitude, location.longitude)
-    }
-
-    private fun gerInputListener(): InputListener = object : InputListener {
-        override fun onMapTap(p0: Map, p1: Point) {
-            binding.mapview.map.deselectGeoObject()
-            Log.e("ssss", p1.toString())
-            endPoint.geometry = p1
-        }
-
-        override fun onMapLongTap(p0: Map, p1: Point) {
-        }
-    }
-
-    private fun getObjectTapListener(): GeoObjectTapListener =
-        GeoObjectTapListener { p0 ->
-            val selectionMetadata: GeoObjectSelectionMetadata = p0
-                .geoObject
-                .metadataContainer
-                .getItem(GeoObjectSelectionMetadata::class.java)
-
-            binding.mapview.map.selectGeoObject(selectionMetadata.id,
-                selectionMetadata.layerId)
-
-            true
-        }
 
     override fun getViewBinding(
         inflater: LayoutInflater,
